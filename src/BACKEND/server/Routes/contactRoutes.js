1392c55f26981e05e80955b4a69c9fcf83159c04
nachjv19 <nachjvasquez@gmail.com>
2025-10-11 14:00:51 -0500

    reade for deploy

--- src/BACKEND/server/Routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { sendContactMail } from "../controllers/ContactController.js";
-
-
-const router = express.Router();
-
-router.post("/", sendContactMail);
-
-export default router;
